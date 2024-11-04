b4dcdae213f2ad197184a78a1bc466c5c68a1771
Giorf81 <rejroberts7@gmail.com>
2024-11-04 13:19:32 +0300

    Добавьте файлы проекта.

---
+++ 6-6-1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace _6_6_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+        class TrafficLight()
+        {
+            private string color;
+
+            private void ChangeColor(string colorr)
+            {
+
+            }
+            public string GetColor()
+            {
+                
+            }
+        }
+
+    }
+}
